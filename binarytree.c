794048892c56df462c36373091dedf67027f28c8
helnantny <helnantnyc@gmail.com>
2024-10-21 14:11:01 +0530

    Update binarytree.c

--- binarytree.c
+++ binarytree.c
@@ -1,53 +1,34 @@
-#include <stdio.h>
-int main()
-{
-    int limit, i, pos;
-    printf("Enter the total number of nodes: ");
-    scanf("%d", &limit);
-    int A[limit];  
-    printf("Enter the node data: ");
-    for (i = 0; i < limit; i++)
-    {
-        scanf("%d", &A[i]);
-    }
-    printf("the formed array nodes\n");
-    for (i = 0; i < limit; i++)
-    {
-        printf("%d\t", A[i]);
-    }
-    printf("\nEnter the position you want to check: ");
-    scanf("%d", &pos);
-    printf("\nthe element at position %d is...%d\n",pos,A[pos]);
-    if (pos < 0 || pos >= limit)
-    {
-        printf("Invalid position\n");
-        return 1;  
-    }
-    if (pos > 0)
-    {
-        printf("Parent of %d is: %d\n", A[pos], A[(pos-1) / 2]);
-    } else
-    {
-        printf("No parent\n");
-    }
-
-
-    if ((2 * pos)+1 >= limit)
-    {
-        printf("No left child\n");
-    }
-    else
-    {
-        printf("Left child of %d is: %d\n", A[pos], A[(2 * pos)+1]);
-    }
-    if ((2 * pos )+ 2 >= limit)
-    {
-        printf("No right child\n");
-    } else
-    {
-        printf("Right child of %d is: %d\n", A[pos], A[(2 * pos )+ 2]);
-    }
-
-    return 0;  
-}
+#include<stdio.h>
+void main(){
+   int limit,i,pos;
+   printf("enter the total number of nodes\n");
+   scanf("%d",&limit);
+   int a[limit];
+   printf("enter the nodes data\n");
+   for(i=1;i<=limit;i++){
+     scanf("%d",&a[i]);
+   }
+   printf("enter the position you want to check\n");
+   scanf("%d",&pos);
+   if((pos/2)!=0){
+     printf("parent of %d is:%d\n",a[pos],a[pos/2]);
+   }
+   else{
+     printf("no parent\n");
+   }
+   if(2*pos>limit){
+      printf("no left child\n");
+   }
+   else{
+     printf("Left child of %d is :%d\n",a[pos],a[2*pos]);
+   }
+   if((2*pos)+1>limit){
+     printf("no right child\n");
+     }
+   else{
+   printf("Right child of %d is:%d\n",a[pos],a[(2*pos)+1]);
+   }
+   }
 	
+   
+ 
